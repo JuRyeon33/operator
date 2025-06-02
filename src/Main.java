1d9d8c06fe9c2bfa3d4f32a0b6cc813e17ecf013
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-02 11:21:00 +0900

    third operator

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,13 @@
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 public class Main {
     public static void main(String[] args) {
-        int a = 10, b = 15;
+        int a = 10, b = 3;
 
-        a += 5;
-        System.out.println(a); // 15
-
-        b -= 5;
-        System.out.println(b); // 10
-
-        b *= 5;
-        System.out.println(b); // 50
-
-        b /= 2;
-        System.out.println(b); // 25
-
-        b %= 3;
-        System.out.println(b); // 1
+        System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(a++);
+        System.out.println(b--);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
