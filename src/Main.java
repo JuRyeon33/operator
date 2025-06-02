ab922411908a55f854f407a40050ed15e0fe3ae7
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-02 10:35:50 +0900

    operator

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main {
+    public static void main(String[] args) {
+        int a = 10, b = 3;
+
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        System.out.println(sum);
+        System.out.println(difference);
+        System.out.println(product);
+        System.out.println(quotient);
+        System.out.println(remainder);
+
+    }
+}
